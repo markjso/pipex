77aa4893e88e09e2e352b8f2e30910588b2c6138
markjso <97271827+markjso@users.noreply.github.com>
2022-08-11 12:32:34 +0930

    Update parse.c

--- parse.c
+++ parse.c
@@ -10,7 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+/* the **get_path function searches for the PATH= variable in the envp using ft_strncmp
+   Places all possible listed paths in the PATH line into a string double array
+   using ft_split and the delimiter ':'. Adds a "/" to the end of each path to
+   make them valid paths using ft_strjoin. To see this in action typ env into a 
+   terminal. This returns all the environment variables. The PATH variable contains
+   a list of paths seperated by a ':' - /usr/local/bin:/usr/bin etc we seperate these
+   with ft_split which then means they are now /usr/local/bin and /usr/bin. To make
+   them valid paths we add a "/" to the end of each one using ft_strjoin to make
+   them a valid path /usr/local/bin/ and /usr/bin/ */
+   
+   #include "pipex.h"
 
 char	**get_path(char **envp)
 {
@@ -35,6 +45,12 @@ char	**get_path(char **envp)
 	}
 	return (env_paths);
 }
+
+/* the *find_cmd_path function looks for the cmd path within the paths returned in
+   our get_path function. First we join the command name to the end of the path using
+   ft_strjoin then test if it is accessible and executable using access to check the path
+   name. We keep looping until we find it in the paths returned in get_path. If we find
+   it return it, otherwise free the path and return NULL./*
 
 char	*find_cmd_path(char *cmd, char **path)
 {
