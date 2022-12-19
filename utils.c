0b2b14994514328fefa6a0523938d893d77b2bb8
markjso <97271827+markjso@users.noreply.github.com>
2022-12-19 11:11:26 +1030

    Update utils.c

--- utils.c
+++ utils.c
@@ -11,12 +11,20 @@
 /* ************************************************************************** */
 
 #include "pipex.h"
+/* Use strerror to return a pointer to the error string describing error
+errnum. Eg No such file or directory*/
 
 void	error_func(void)
 {
 	write(2, strerror(errno), ft_strlen(strerror(errno)));
 	exit(EXIT_FAILURE);
 }
+/* checks the INFILE exists (F_OK) and if it can be accessed (X_OK)
+and returns an error if either is false. If true it opens the file
+with read only access (O_RDONLY). If it is not the INFILE and it does 
+not exist create a new file (O_CREAT) with read/write access(O_RDWR).
+If the file exists O_TRUNC will truncate the length to 0 and give the file
+owner read/write privelages (0644) */
 
 int	open_file(char *file, int flag)
 {
